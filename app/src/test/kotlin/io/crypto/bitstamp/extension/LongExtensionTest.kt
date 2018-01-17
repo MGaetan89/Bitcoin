ef5f7df7c403749c933abdd9ffc810ce1bd71b34
Gaëtan Muller <m.gaetan89@gmail.com>
2018-01-17 20:36:08 +0100

    Fix tests

--- app/src/test/kotlin/io/crypto/bitstamp/extension/LongExtensionTest.kt
+++ app/src/test/kotlin/io/crypto/bitstamp/extension/LongExtensionTest.kt
@@ -4,11 +4,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import java.util.Locale
+import java.util.TimeZone
 
 class LongExtensionTest {
 	@Before
 	fun before() {
 		Locale.setDefault(Locale.US)
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
 	}
 
 	@Test
@@ -20,15 +22,15 @@ class LongExtensionTest {
 
 	@Test
 	fun toFormattedDateTime() {
-		assertThat(0L.toFormattedDateTime()).isEqualTo("Jan 1, 1970 1:00:00 AM")
-		assertThat(1516140164L.toFormattedDateTime()).isEqualTo("Jan 16, 2018 11:02:44 PM")
-		assertThat(5532594234L.toFormattedDateTime()).isEqualTo("Apr 27, 2145 5:43:54 PM")
+		assertThat(0L.toFormattedDateTime()).isEqualTo("Jan 1, 1970 12:00:00 AM")
+		assertThat(1516140164L.toFormattedDateTime()).isEqualTo("Jan 16, 2018 10:02:44 PM")
+		assertThat(5532594234L.toFormattedDateTime()).isEqualTo("Apr 27, 2145 3:43:54 PM")
 	}
 
 	@Test
 	fun toFormattedTime() {
-		assertThat(0L.toFormattedTime()).isEqualTo("1:00:00 AM")
-		assertThat(1516140164L.toFormattedTime()).isEqualTo("11:02:44 PM")
-		assertThat(5532594234L.toFormattedTime()).isEqualTo("5:43:54 PM")
+		assertThat(0L.toFormattedTime()).isEqualTo("12:00:00 AM")
+		assertThat(1516140164L.toFormattedTime()).isEqualTo("10:02:44 PM")
+		assertThat(5532594234L.toFormattedTime()).isEqualTo("3:43:54 PM")
 	}
 }
--- app/src/test/kotlin/io/crypto/bitstamp/extension/StringExtensionTest.kt
+++ app/src/test/kotlin/io/crypto/bitstamp/extension/StringExtensionTest.kt
@@ -1,14 +1,21 @@
 package io.crypto.bitstamp.extension
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
+import java.util.TimeZone
 
 class StringExtensionTest {
+	@Before
+	fun before() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+	}
+
 	@Test
 	fun toTimestamp() {
 		assertThat("".toTimestamp()).isNull()
 		assertThat("2018-01-16".toTimestamp()).isNull()
 		assertThat("23:02:44".toTimestamp()).isNull()
-		assertThat("2018-01-16 23:02:44".toTimestamp()).isEqualTo(1516140164L)
+		assertThat("2018-01-16 23:02:44".toTimestamp()).isEqualTo(1516143764L)
 	}
 }
