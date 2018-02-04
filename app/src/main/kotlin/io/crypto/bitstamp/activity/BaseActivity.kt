43fd5be0d36e16a3eb871a40cc9fe35734167b6b
Gaëtan Muller <m.gaetan89@gmail.com>
2018-02-04 17:56:06 +0100

    Remove `runPeriodically` method

--- app/src/main/kotlin/io/crypto/bitstamp/activity/BaseActivity.kt
+++ app/src/main/kotlin/io/crypto/bitstamp/activity/BaseActivity.kt
@@ -18,17 +18,9 @@ import kotlinx.android.synthetic.main.activity_base.activity_stub
 import kotlinx.android.synthetic.main.activity_base.bottom_navigation
 import kotlinx.android.synthetic.main.activity_base.tabs
 import kotlinx.android.synthetic.main.activity_base.toolbar
-import kotlinx.coroutines.experimental.Job
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
-import java.util.concurrent.TimeUnit
 
 abstract class BaseActivity : AppCompatActivity(), ViewStub.OnInflateListener,
 	BottomNavigationView.OnNavigationItemSelectedListener {
-	private val jobs = mutableListOf<Job>()
-
-	private var running = false
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
@@ -74,20 +66,6 @@ abstract class BaseActivity : AppCompatActivity(), ViewStub.OnInflateListener,
 		}
 	}
 
-	override fun onPause() {
-		this.running = false
-
-		this.clearJobs()
-
-		super.onPause()
-	}
-
-	override fun onResume() {
-		super.onResume()
-
-		this.running = true
-	}
-
 	protected open fun configureTabs(tabs: TabLayout) = false
 
 	@LayoutRes
@@ -97,46 +75,6 @@ abstract class BaseActivity : AppCompatActivity(), ViewStub.OnInflateListener,
 	protected abstract fun getSelectedTabId(): Int
 
 	protected abstract fun getToolbarTitle(): CharSequence
-
-	protected fun runPeriodically(
-		time: Long = 2L,
-		unit: TimeUnit = TimeUnit.SECONDS,
-		action: () -> List<Job>
-	) {
-		if (this.running) {
-			launch {
-				val jobs = action()
-
-				this@BaseActivity.jobs.addAll(jobs)
-
-				jobs.forEach {
-					if (running) {
-						it.join()
-					} else {
-						it.cancel()
-					}
-				}
-
-				synchronized(this@BaseActivity.jobs) {
-					this@BaseActivity.jobs.clear()
-				}
-
-				if (running) {
-					delay(time, unit)
-
-					runPeriodically(time, unit, action)
-				}
-			}
-		}
-	}
-
-	private fun clearJobs() {
-		this.jobs.forEach {
-			it.cancel()
-		}
-
-		this.jobs.clear()
-	}
 
 	private fun configureBottomNavigation() {
 		this.bottom_navigation.let {
